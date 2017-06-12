46f22d8b4c0e40949831eb8c79c659db995d64dc
lxxhack <1572411660@qq.com>
2017-06-12 22:59:56 +0800

    Create main.js

--- main/main.js
+++ main/main.js
@@ -1,3 +1,11 @@
-module.exports = function main() {
+module.exports = function main(str) {
+  var ch;
+  var sum=0;
+  for (var i=0;i<str.length-1;i++)
+  {
+    ch=str.charAt(i);
+    sum+=parseInt(ch);
+  }
+  return sum;
   // Write your cade here
 };
